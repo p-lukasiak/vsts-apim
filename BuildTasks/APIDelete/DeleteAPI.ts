2aadcdcbd083a982425269f538cc877b1590d678
Pawel Lukasiak <pawel.lukasiak@tatasteel.com>
2017-11-24 20:41:17 +0100

    Testing the build tasks reference and script execution

--- BuildTasks/APIDelete/DeleteAPI.ts
+++ BuildTasks/APIDelete/DeleteAPI.ts
@@ -1,10 +1,22 @@
 import * as tl from "vsts-task-lib/task";
 import * as common from "./Common";
 
+const httpMethod = "DELETE";
 const serviceConnectionFieldName = "connectedServiceName";
 
+// Retrive API details
 const apiName = tl.getInput("apiName", true);
-const outputVariable = tl.getInput("outputVariable", true);
+const subscriptionId = tl.getInput("subscriptionId", true);
+const resourceGroupName = tl.getInput("resourceGroupName", true);
+const serviceName = tl.getInput("serviceName", true);
+const apiVersion = tl.getInput("apiVersion", true);
 
-let serviceConnection = common.getAPIMManagementEndpointDetails(serviceConnectionFieldName);
-tl.setVariable(outputVariable, serviceConnection.identifier);
+// Retrive API Management connection details
+const serviceConnection = common.getAPIMManagementEndpointDetails(serviceConnectionFieldName);
+
+// Create the request URL
+const restResource = "/apis/" + apiName;
+
+// Send the request to API Management
+const headers = JSON.parse("{''}");
+common.restCall(serviceConnection.url, restResource, serviceConnection.identifier, serviceConnection.key, httpMethod, "", headers);
--- BuildTasks/Common/Common.ts
+++ BuildTasks/Common/Common.ts
@@ -1,35 +1,49 @@
 import tl = require("vsts-task-lib");
 import request = require("request");
+import crypto = require("crypto-js");
 import * as tasklib from "vsts-task-lib/task";
+import { toBase64 } from "request/lib/helpers";
 
-export function restCall(webServiceUrl: string, httpVerb: string, body: string, useBasicAuthentication: Boolean, username: string, password: string, contentType: string, timeout: number, allowInvalidSSLCertificate: Boolean, headers: any) {
-    let useStrictSSL = !allowInvalidSSLCertificate;
+function createAccessToken(identifier: string, key: string): string {
+    // Create the proper string format of the datetime for access token (valid for 1 minute)
+    let expiryDate = new Date();
+    expiryDate.setMinutes(expiryDate.getMinutes() + 1);
 
-    if (useBasicAuthentication) {
-        request({
-            uri: webServiceUrl,
-            // qs: { force: true }, //Query string data
-            method: httpVerb,
-            headers: {"content-type": contentType, ...headers},
-            body: body,
-            auth: {
-                user: username,
-                pass: password
-            },
-            timeout: timeout,
-            strictSSL: useStrictSSL
-        }, this.callBack.bind(this));
-    }
-    else {
-        request({
-            uri: webServiceUrl,
-            method: httpVerb,
-            headers: {"content-type": contentType, ...headers},
-            body: body,
-            timeout: timeout,
-            strictSSL: useStrictSSL
-        }, this.callBack.bind(this));
-    }
+    const expiry = expiryDate.getDate()
+    + (expiryDate.getMonth() + 1)
+    + expiryDate.getFullYear()
+    + expiryDate.getHours()
+    + expiryDate.getMinutes()
+    + expiryDate.getSeconds();
+    tl.debug("Using expiry date" + expiry);
+
+    const dataToSign = identifier + "\n" + expiry;
+    const signature = toBase64(crypto.HmacSHA512(dataToSign, key));
+    return "SharedAccessSignature uid=" + identifier + "&ex=" + expiry + "&sn=" + signature;
+}
+
+export function restCall(serviceConnection: any, restResource:string, httpVerb: string, body: string, headers: any) {
+
+    // Create access token 
+    const accessToken = createAccessToken(serviceConnection.identifier, serviceConnection.key);
+
+    // Compose REST endpoint
+    const restEndpoint = serviceConnection.url
+    + "/subscriptions/" + serviceConnection.subscriptionId
+    + "/resourceGroups/" + serviceConnection.resourceGroup
+    + "/providers/Microsoft.ApiManagement"
+    + "/service/" + serviceConnection.serviceName
+    + restResource
+    + "?api-version=" + serviceConnection.apiVersion; 
+
+    request({
+        uri: restEndpoint,
+        method: httpVerb,
+        headers: {"Authorization": accessToken, ...headers},
+        body: body,
+        timeout: 20,
+        strictSSL: true
+    }, this.callBack.bind(this));
 }
 
 function callBack(error, response, body) {
@@ -50,16 +64,26 @@ function callBack(error, response, body) {
 }
 
 export function getAPIMManagementEndpointDetails(inputFieldName): any {
+
+    const apiVersion = "2017-03-01";
+    
     const apimMgmtEndpoint = tasklib.getInput(inputFieldName, true);
 
     const hostUrl = tasklib.getEndpointUrl(apimMgmtEndpoint, false);
     const auth = tasklib.getEndpointAuthorization(apimMgmtEndpoint, false);
     const identifier = auth.parameters["identifier"];
     const key = auth.parameters["key"];
+    const subscriptionId = auth.parameters["subscriptionId"];
+    const resourceGroup = auth.parameters["resourceGroup"];
+    const service = auth.parameters["service"];
 
     return {
         "url": hostUrl,
         "identifier": identifier,
-        "key": key
+        "key": key,
+        "subscriptionId": subscriptionId,
+        "resourceGroup": resourceGroup,
+        "service": service,
+        "apiVersion": apiVersion
     };
 }
